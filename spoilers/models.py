67c9ef737fbfdaacd7dcbb392fe11c529cd3a158
Zach Tibbitts <zachtib@gmail.com>
2020-01-02 14:10:27 -0500

    Unique constraints

--- spoilers/models.py
+++ spoilers/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class MagicSet(models.Model):
-    scryfall_id = models.UUIDField()
+    scryfall_id = models.UUIDField(unique=True)
     code = models.CharField(max_length=20)
     name = models.CharField(max_length=200)
     release_date = models.DateField()
@@ -13,6 +13,9 @@ class MagicSet(models.Model):
 
 
 class MagicCard(models.Model):
-    scryfall_id = models.UUIDField()
+    scryfall_id = models.UUIDField(unique=True)
     name = models.CharField(max_length=200)
     magic_set = models.ForeignKey(MagicSet, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
