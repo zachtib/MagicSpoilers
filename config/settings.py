d100cc07b770601210fd563f40f46736aafe8fe7
Zach Tibbitts <zachtib@gmail.com>
2020-01-09 08:34:20 -0500

    read secret key from environ

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd9g&*^o$k854)=c-(zp)@rryjsyjkdclx!qww7^brk7$gs+kd5'
+SECRET_KEY = os.envrion.get('SECRET_KEY', 'd9g&*^o$k854)=c-(zp)@rryjsyjkdclx!qww7^brk7$gs+kd5')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', '0') == '1'
